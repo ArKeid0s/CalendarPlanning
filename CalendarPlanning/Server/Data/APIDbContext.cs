05512b0734bae516e3013ed792fc14ec5b6188b8
arkeid0s <matproz.gaming@gmail.com>
2023-06-04 00:36:32 +0200

    Added ContextSeed to have defauld values

--- CalendarPlanning/Server/Data/APIDbContext.cs
+++ CalendarPlanning/Server/Data/APIDbContext.cs
@@ -15,6 +15,13 @@ namespace CalendarPlanning.Server.Data
                 {
                     if (!databaseCreator.CanConnect()) databaseCreator.Create();
                     if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+
+                    // Init default values for test purposes
+                    var contextSeed = new ContextSeed();
+                    if (!contextSeed.HasData(this))
+                    {
+                        contextSeed.Seed(this);
+                    }
                 }
             }
             catch (Exception ex)
@@ -23,10 +30,32 @@ namespace CalendarPlanning.Server.Data
             }
         }
 
-        public required DbSet<Employee> Employees { get; set; }
-        public required DbSet<Holiday> Holidays { get; set; }
-        public required DbSet<Schedule> Schedules { get; set; }
-        public required DbSet<Shift> Shifts { get; set; }
-        public required DbSet<Store> Stores { get; set; }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Shift>()
+        //        .HasOne(s => s.Employee)
+        //        .WithMany(e => e.Shifts)
+        //        .OnDelete(DeleteBehavior.Restrict); // Prevents cascading deletes from Employee to Shift but implies that the shift deletion has to be done manually
+
+        //    modelBuilder.Entity<Shift>()
+        //        .HasOne(s => s.Schedule)
+        //        .WithMany(sc => sc.Shifts)
+        //        .OnDelete(DeleteBehavior.Restrict); // Prevents cascading deletes from Schedule to Shift but implies that the shift deletion has to be done manually
+
+        //    modelBuilder.Entity<Employee>()
+        //        .HasOne(e => e.Store)
+        //        .WithMany(s => s.Employees)
+        //        .OnDelete(DeleteBehavior.Restrict);
+
+        //}
+
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Holiday> Holidays { get; set; } = null!;
+        public DbSet<Schedule> Schedules { get; set; } = null!;
+        public DbSet<Shift> Shifts { get; set; } = null!;
+        public DbSet<Store> Stores { get; set; } = null!;
     }
 }
---
+++ CalendarPlanning/Server/Data/ContextSeed.cs
@@ -0,0 +1,78 @@
+﻿using CalendarPlanning.Shared.Models;
+
+namespace CalendarPlanning.Server.Data
+{
+    public class ContextSeed
+    {
+        public List<Employee> employees = new();
+        public List<Store> stores = new();
+
+        public ContextSeed()
+        {
+        }
+
+        public void Seed(APIDbContext context)
+        {
+            InitStore();
+
+            if (!context.Stores.Any() && stores.Count != 0)
+            {
+                context.Stores.AddRange(stores);
+                context.SaveChanges();
+            }
+
+            InitEmployees(context);
+
+            if (!context.Employees.Any() && employees.Count != 0)
+            {
+                context.Employees.AddRange(employees);
+                context.SaveChanges();
+            }
+        }
+
+        public bool HasData(APIDbContext context)
+        {
+            return context.Stores.Any() && context.Employees.Any();
+        }
+
+        private void InitEmployees(APIDbContext context)
+        {
+            employees = new List<Employee>
+            {
+                new Employee
+                {
+                    EmployeeId = Guid.NewGuid(),
+                    FirstName = "John",
+                    LastName = "Doe",
+                    StoreId = context.Stores.First(s => s.Name == "Store 1").StoreId
+                },
+                new Employee
+                {
+                    EmployeeId = Guid.NewGuid(),
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    StoreId = context.Stores.First(s => s.Name == "Store 2").StoreId
+                }
+            };
+        }
+
+        private void InitStore()
+        {
+            stores = new List<Store>
+            {
+                new Store
+                {
+                    StoreId = Guid.NewGuid(),
+                    Name = "Store 1",
+                    Address = "Address 1"
+                },
+                new Store
+                {
+                    StoreId = Guid.NewGuid(),
+                    Name = "Store 2",
+                    Address = "Address 2"
+                }
+            };
+        }
+    }
+}
