45494d7f8e206264d21703de6d87ddddb0d49f7f
Mathis ROCHER <m.rocher@groupeonepoint.com>
2023-07-30 23:06:21 +0200

    Added radzen

--- CalendarPlanning/Client/Program.cs
+++ CalendarPlanning/Client/Program.cs
@@ -6,6 +6,7 @@ using CalendarPlanning.Client.Utils;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,6 +17,13 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IIncentivesService, IncentivesService>();
+
+// === RADZEN ===
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+// === RADZEN ===
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
