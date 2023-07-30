dbf54ae2be8565d6f7b51d689b31dd9121f01b0f
Mathis ROCHER <m.rocher@groupeonepoint.com>
2023-07-30 23:06:35 +0200

    Created EmployeesService

---
+++ CalendarPlanning/Client/Services/EmployeesService.cs
@@ -0,0 +1,21 @@
+﻿using CalendarPlanning.Client.Services.Interfaces;
+using CalendarPlanning.Shared.Models.DTO;
+using System.Net.Http.Json;
+
+namespace CalendarPlanning.Client.Services
+{
+    public class EmployeesService : IEmployeesService
+    {
+        private readonly HttpClient _http;
+
+        public EmployeesService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<EmployeeDto>?> GetAllAsync()
+        {
+            return await _http.GetFromJsonAsync<List<EmployeeDto>>("api/Employees");
+        }
+    }
+}
---
+++ CalendarPlanning/Client/Services/Interfaces/IEmployeesService.cs
@@ -0,0 +1,9 @@
+﻿using CalendarPlanning.Shared.Models.DTO;
+
+namespace CalendarPlanning.Client.Services.Interfaces
+{
+    public interface IEmployeesService
+    {
+        Task<List<EmployeeDto>?> GetAllAsync();
+    }
+}
